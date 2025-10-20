9595953d6d45d3c912b05896aeb279db97edaa6b
dev_wandry <dima.development@wandry.com.ua>
2025-10-20 20:56:09 +0300

    wip landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import {
   PageHeaderHeading,
 } from "@/components/page-header";
 import { Button } from "@/components/ui/button";
+import { HeroCodeblock } from "@/components/hero-codeblock";
+import { cn } from "@/lib/utils";
 
 const title = "Simplified Form Components for Inertia.js & React.js";
 const description =
@@ -15,17 +17,34 @@ const description =
 
 export default function Home() {
   return (
-    <PageHeader>
-      <PageHeaderHeading className="max-w-4xl">{title}</PageHeaderHeading>
-      <PageHeaderDescription>{description}</PageHeaderDescription>
-      <PageActions>
-        <Button asChild size="sm">
-          <Link href="/docs/installation">Get Started</Link>
-        </Button>
-        <Button asChild size="sm" variant="ghost">
-          <Link href="/docs/components">View Components</Link>
-        </Button>
-      </PageActions>
-    </PageHeader>
+    <div className="relative flex flex-col flex-1 h-full w-full items-center justify-center bg-white dark:bg-black">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]"
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+
+      <div className="z-10">
+        <PageHeader>
+          <PageHeaderHeading className="max-w-4xl">{title}</PageHeaderHeading>
+          <PageHeaderDescription>{description}</PageHeaderDescription>
+          <PageActions>
+            <Button asChild size="sm">
+              <Link href="/docs/installation">Get Started</Link>
+            </Button>
+            <Button asChild size="sm" variant="ghost">
+              <Link href="/docs/components">View Components</Link>
+            </Button>
+          </PageActions>
+        </PageHeader>
+        <div className="container-wrapper">
+          <HeroCodeblock />
+        </div>
+      </div>
+    </div>
   );
 }
