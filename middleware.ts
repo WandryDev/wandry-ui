b6134ba555ee5f345a6f92dab279da699666074b
dev_wandry <dima.development@wandry.com.ua>
2025-11-03 20:52:06 +0200

    test wandry analytics

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import { isMarkdownPreferred, rewritePath } from "fumadocs-core/negotiation";
 
+import { captureRegistryEvent } from "@wandry/analytics-sdk";
+
 const { rewrite: rewriteLLM } = rewritePath("/docs/*path", "/llms.mdx/*path");
 
 export function middleware(request: NextRequest) {
   // import {captureRegistryEvents} from '@wandry/analytics-sdk';
   // captureRegistryEvents(request, token);
+
+  captureRegistryEvent(
+    request,
+    "8|OOL1rO2LRehDNYDSN1WbegXAX9WlxcbPymsug1Qkebadd119"
+  );
 
   if (isMarkdownPreferred(request)) {
     const result = rewriteLLM(request.nextUrl.pathname);
